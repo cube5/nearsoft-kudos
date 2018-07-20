050fd8fe71339d3d1023c7c0eda8a11c6d5f2670
Daniel Sanchez <jdaniel.saed@gmail.com>
2018-07-20 17:08:47 -0500

    [WIP] add serviceWorker notifications

--- src/index.js
+++ src/index.js
@@ -6,4 +6,11 @@ import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
+registerServiceWorker({
+  onContentAvailable: () => {
+    // the application should show anotification saying "New content is available; please refresh."
+  },
+  onContentCached: () => {
+    // the application should show anotification saying "Content is cached for offline use."
+  }
+});
