4ac566a02390324e6ae2a666b570dd31542ad3f6
Daniel Sanchez <jdaniel.saed@gmail.com>
2018-07-20 14:49:35 -0500

    unused code

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,8 +1,11 @@
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core";
 
-// A function you can extract and put into its own module.
-// Yes, 11 lines of code is all you need.
+/**
+ * @see https://material-ui.com/customization/css-in-js/#render-props-api-11-lines-
+ * @param {object} styles
+ * @param {object} options
+ */
 export function createStyled(styles, options) {
   function Styled(props) {
     const { children, ...other } = props;
@@ -13,14 +16,4 @@ export function createStyled(styles, options) {
     classes: PropTypes.object.isRequired
   };
   return withStyles(styles, options)(Styled);
-}
-
-/**
- * Returns millimeters converted to pixels.
- * 1 mm = 3.7795275591 px
- * @see https://www.unitconverters.net/typography/millimeter-to-pixel-x.htm
- * @param {number} millimeters
- */
-export function millimetersToPixels(millimeters = 0) {
-  return millimeters * 3.7795275591;
 }
